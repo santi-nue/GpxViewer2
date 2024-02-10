a7267d9d58d909e5b45f54860e279df942cf40b2
Roland König <roland.koenig@rolandk.de>
2024-02-10 22:57:09 +0800

    Updated RouteSelectionView

--- src/GpxViewer2/Program.cs
+++ src/GpxViewer2/Program.cs
@@ -1,12 +1,16 @@
 ﻿using Avalonia;
 using System;
+using GpxViewer2.Model.GpxXmlExtensions;
 using GpxViewer2.Services;
 using GpxViewer2.Services.GpxFileStore;
 using GpxViewer2.Services.RecentlyOpened;
 using GpxViewer2.UseCases;
 using GpxViewer2.Views;
 using Microsoft.Extensions.DependencyInjection;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
 using RolandK.AvaloniaExtensions.DependencyInjection;
+using RolandK.Formats.Gpx;
 using RolandK.InProcessMessaging;
 
 namespace GpxViewer2;
@@ -22,7 +26,16 @@ class Program
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        IconProvider.Current
+            .Register<FontAwesomeIconProvider>();
+        
+        // Register GpxFile extensions
+        GpxFile.RegisterExtensionType(typeof(TrackExtension));
+        GpxFile.RegisterExtensionType(typeof(RouteExtension));
+        GpxFile.RegisterNamespace("rkgpxv", "http://gpxviewer.rolandk.net/");
+        
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace()
@@ -35,16 +48,17 @@ class Program
                 services.AddSingleton<IRecentlyOpenedFilesService>(
                     _ => new RecentlyOpenedFilesService(".RKMediaGallery", 5));
                 services.AddSingleton<IGpxFileRepositoryService, GpxFileRepositoryService>();
-                
+
                 // ViewModels
                 services.AddTransient<MainWindowViewModel>();
                 services.AddTransient<MapViewModel>();
                 services.AddTransient<RouteDetailViewModel>();
                 services.AddTransient<RouteSelectionViewModel>();
-                
+
                 // UseCases
                 services.AddScoped<LoadGpxFileUseCase>();
                 services.AddScoped<LoadGpxDirectoryUseCase>();
                 services.AddScoped<SelectGpxToursUseCase>();
             });
+    }
 }
--- src/GpxViewer2/Views/RouteSelection/RouteSelectionNode.cs
+++ src/GpxViewer2/Views/RouteSelection/RouteSelectionNode.cs
@@ -1,5 +1,8 @@
 using System.Collections.ObjectModel;
+using GpxViewer2.Model;
+using GpxViewer2.Model.GpxXmlExtensions;
 using GpxViewer2.Services.GpxFileStore;
+using GpxViewer2.Util;
 
 namespace GpxViewer2.Views.RouteSelection;
 
@@ -7,12 +10,51 @@ public class RouteSelectionNode
 {
     public ObservableCollection<RouteSelectionNode> ChildNodes { get; } = new();
 
+    public LoadedGpxFileTourInfo? AssociatedTour { get; }
+    
     public GpxFileRepositoryNode Node { get; }
+
+    public bool HasAssociatedTour => AssociatedTour != null;
+    
+    public bool IsTourFinishedVisible 
+        => this.AssociatedTour?.RawTourExtensionData.State == GpxTrackState.Succeeded;
+
+    public double DistanceKm
+        => this.AssociatedTour?.DistanceKm ?? 0.0;
+
+    public double ElevationUpMeters
+        => this.AssociatedTour?.ElevationUpMeters ?? 0.0;
+
+    public double ElevationDownMeters
+        => this.AssociatedTour?.ElevationDownMeters ?? 0.0;
+
+    public string TooltipText
+    {
+        get
+        {
+            using var scope = PooledStringBuilders.Current.UseStringBuilder(out var stringBuilder);
+
+            stringBuilder.Append(this.Node.NodeText);
+            if (this.AssociatedTour != null)
+            {
+                stringBuilder.Append(", ");
+                stringBuilder.Append(this.AssociatedTour.DistanceKm.ToString("N1"));
+                stringBuilder.Append(" km, ");
+                stringBuilder.Append(this.AssociatedTour.ElevationUpMeters.ToString("N0"));
+                stringBuilder.Append(" m up, ");
+                stringBuilder.Append(this.AssociatedTour.ElevationDownMeters.ToString("N0"));
+                stringBuilder.Append(" m down");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
 
     public RouteSelectionNode(GpxFileRepositoryNode node)
     {
         this.Node = node;
-
+        this.AssociatedTour = node.GetAssociatedTour();
+        
         foreach (var actChildNode in node.ChildNodes)
         {
             this.ChildNodes.Add(new RouteSelectionNode(actChildNode));
