6f714f25107956bd2500386e07d4632b4ce34d03
Roland König <roland.koenig@rolandk.de>
2024-03-24 22:30:32 +0100

    Migrated code style configuration to .editorconfig

--- src/GpxViewer2/Program.cs
+++ src/GpxViewer2/Program.cs
@@ -17,7 +17,7 @@ using RolandK.InProcessMessaging;
 
 namespace GpxViewer2;
 
-class Program
+public static class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
@@ -42,6 +42,7 @@ class Program
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
+    // ReSharper disable once MemberCanBePrivate.Global
     public static AppBuilder BuildAvaloniaApp(string[] args)
     {
         IconProvider.Current
